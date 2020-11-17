202c3806657d43a55b3230eb3359015d15a24ecf
Eric <eric.m.lombardo@gmail.com>
2020-11-17 10:17:54 -0500

    commit tree 4fa5be4a7c8b8dc03e761d63c9292a3463cb56e1

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
 def my_find(collection)
+  i = 0   # set counter
 
+  while i < collection.length   # loop through length of array
+    return collection[i] if yield(collection[i])    # if yield return is true => return value of what was yielded
+    i += 1    # increment counter by 1
+  end
 end
